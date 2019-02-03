fa0d39e35f52640f5a993c568c15d3d56591f99c
Jonas Körner <jkoerner@odd-it.com>
2019-02-03 21:33:02 +0100

    Initial commit

---
+++ ammooverview.py
@@ -0,0 +1,59 @@
+import api
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+import math
+ammoinfo = api.cached('./ammo.json',api.get_ammo_info)
+ammoinfo = pd.DataFrame(ammoinfo)
+print(ammoinfo)
+alltraders = list(filter(lambda x:x.startswith('Trader:'),list(ammoinfo.columns)))
+print(alltraders)
+categories = list(set(ammoinfo["category"]))
+sqrtcats = math.ceil(math.sqrt(len(categories)))
+for categoryID in range(len(categories)):
+    category = categories[categoryID]
+    fig = plt.figure()
+    diagram = plt.subplot(1,1,1)
+    subset = ammoinfo[ammoinfo.category == category]
+    diagram.set_title(category)
+    xattr='penetration'
+    yattr='damage'
+
+    subset[xattr] = pd.to_numeric(subset[xattr])
+    subset[yattr] = pd.to_numeric(subset[yattr])
+
+    traderCount = 0
+    print(subset)
+    for trader in alltraders:
+        traderSubset = subset[subset[trader] == True]
+        x = traderSubset[xattr]
+        y = traderSubset[yattr]
+        if(traderSubset.size>0):
+            diagram.scatter(x,y,label=trader)
+        traderCount +=1
+
+    for i, row in subset.iterrows():
+        t = row['name']
+        xx = row[xattr]
+        yy = row[yattr]
+        diagram.annotate(t,(xx,yy),size=8,ha='left',va='bottom',rotation=30)
+    
+    xmin = subset[xattr].min()
+    xmax = subset[xattr].max()
+
+    ymin = subset[yattr].min()
+    ymax = subset[yattr].max()
+
+    axes = plt.gca()
+    axes.set_xlim([xmin,xmax])
+    axes.set_ylim([ymin,ymax])
+
+    plt.xlabel(xattr)
+    plt.ylabel(yattr)
+    diagram.legend()
+    plt.show(block=False)
+    
+    
+    
+
+plt.show()
---
+++ api.py
@@ -0,0 +1,86 @@
+import requests
+import re
+import json
+import os
+
+def http_get(request,payload):
+    r =requests.get(request,params=payload)
+    return r
+
+def get_api(payload):
+    page_url = "https://escapefromtarkov.gamepedia.com/api.php"
+    return http_get(page_url,payload)
+
+def get_page(page,prop,additionalPayload = {}):
+    payload = {'action':'parse','page':page,'format':'json','prop':prop,**additionalPayload}
+
+    return get_api(payload)
+
+def serialize(filename,data):
+    data = json.dumps(data)
+    with open(filename,'w') as f:
+        f.write(data)
+
+def deserialize(filename):
+    with open(filename,'r') as f:
+        return json.load(f)
+
+def cached(file,fn):
+    if os.path.isfile(file):
+        return deserialize(file)
+
+    data = fn()
+    serialize(file,data)
+    return data
+
+def get_ammo_info():
+    ammopage = get_page("Ammunition",'links').json()['parse']['links']
+    infobox_regex = re.compile("\{\{Infobox ammo(.*)\}\}",re.DOTALL)
+    dataname_regex = re.compile('\|[a-z]+\s*=')
+    data_regex = re.compile('=(.*)$')
+
+    ammos = []
+    for maybeAmmo in ammopage:
+        maybeAmmoName = maybeAmmo['*']
+        categories = get_page(maybeAmmoName,'categories')
+        categoryNames = list(map(lambda x:x['*'],categories.json()['parse']['categories']))
+        
+        if 'Ammo' in categoryNames:
+            templateName = 'Template:Infobox ammo'
+            content = get_page(maybeAmmoName,'wikitext').json()['parse']['wikitext']['*']
+            infobox = infobox_regex.findall(content)[0].split("\n")
+            valueDict = {}
+            for infobox_line in infobox:
+               
+                datatypeNames = dataname_regex.findall(infobox_line)
+                datas = data_regex.findall(infobox_line)
+                if  len(datatypeNames) == 1 and len(datas) == 1:
+                    datatypeName = datatypeNames[0].replace('|','').replace('=','').strip()
+                    data = datas[0].replace('=','').strip()
+                    
+                    valueDict[datatypeName]= data
+            valueDict['category'] = categoryNames[0]
+            valueDict['name']=maybeAmmoName
+            ammos.append(valueDict)
+    traderslevels = set()
+    for ammo in ammos:
+           traderstring = ammo['trader']
+           traders = traderstring.split('<br/>')
+           for t in traders:
+               traderslevels.add(t)
+    print('Traders:',traderslevels)
+    traderslevels = list(traderslevels)
+    for ammo in ammos:
+           traderstring = ammo['trader']
+           traders = traderstring.split('<br/>')
+           for trader in traderslevels:
+               ammo['Trader:' +trader] = trader in traders
+
+    return ammos
+   
+def find_common_start(list_of_strings):
+    list_of_strings = list(set(list_of_strings))
+    if len(list_of_strings== 0):
+        return []
+    abort = False
+    commonstring = ""
